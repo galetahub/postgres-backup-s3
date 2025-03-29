b10f2f34f57d9c71aadb1a0c27068cd2964e6bfa
Igor Galeta <galeta.igor@gmail.com>
2025-03-29 22:50:11 +0200

    chore: check if S3_PREFIX var exists

--- src/backup.sh
+++ src/backup.sh
@@ -3,12 +3,18 @@
 set -e
 set -o pipefail
 
-timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
-s3_uri_base="s3://${S3_BUCKET}/${S3_PREFIX}/${POSTGRES_DATABASE}_${timestamp}.dump"
-
 if [ -z "$PGDUMP_EXTRA_OPTS" ]; then
   export PGDUMP_EXTRA_OPTS=""
 fi
+
+if [ -z "$S3_PREFIX" ]; then
+  export DESTINATION="s3://${S3_BUCKET}"
+else
+  export DESTINATION="s3://${S3_BUCKET}/${S3_PREFIX}"
+fi
+
+timestamp=$(date +"%Y-%m-%dT%H:%M:%S")
+s3_uri_base="${DESTINATION}/${POSTGRES_DATABASE}_${timestamp}.dump"
 
 echo -e "\033[33mCreating backup of $POSTGRES_DATABASE database...\033[0m"
 echo -e "\033[33mDestination: $s3_uri_base\033[0m"
